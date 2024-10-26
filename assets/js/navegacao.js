41264c147e0fa62e75070e644fb4130c48a7358e
Victor Leonam <169005086+VicLeonam1@users.noreply.github.com>
2024-10-26 17:04:15 -0300

    navegação

---
+++ assets/js/navegacao.js
@@ -0,0 +1,22 @@
+let overlay = document.querySelector('.overlay-menu');
+let navegacao = document.querySelector('.navegacao');
+
+function menu() {
+    if (navegacao.style.left === "0px") {
+        navegacao.style.left = "-100%";
+        overlay.style.opacity = '0'; 
+    } else {
+        navegacao.style.left = "0";
+        overlay.style.opacity = '1'; 
+    }
+}
+
+function exit() {
+    if (navegacao.style.left === "0px") {
+        navegacao.style.left = "-100%";
+        overlay.style.opacity = '0'; 
+    } else {
+        navegacao.style.left = "0";
+        overlay.style.opacity = '1'; 
+    }
+}
